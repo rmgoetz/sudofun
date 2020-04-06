db87036b1cc5d9ec461672b3cc8d5c3efa73f141
rmgoetz <32755225+rmgoetz@users.noreply.github.com>
2020-04-06 04:10:05 -0400

    tidying and adding psuedo-random to sf_make_funcs

--- sudofun/sf_make_funcs.py
+++ sudofun/sf_make_funcs.py
@@ -14,3 +14,21 @@ This is currently a work in progress
 
 last update: April 5, 2020 
 """
+
+def rando(mod=9,sleep=False):
+    '''
+    A quick function for generating psuedo-random numbers. A work in progress.
+    Outputs from rando calls made within ~ 1e-5 seconds of one another may be
+    correlated.
+    '''
+    t = time.time_ns()
+    if sleep:
+        time.sleep(1e-7) # calling this function kills more time than the value
+    a = (t//100) % 10000
+    b = a ^ 10922
+    c = a ^ 5461
+    d = a ^ 3276
+    e = a ^ 13107
+    f = (b<<(e % 3))*(c<<(b % 5))*(d<<(c % 7))*(e<<(d % 11))
+    g = int(str(f)[:4])
+    return (g % mod)+1
